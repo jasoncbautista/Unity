d30c337c8be6e2a63fdf784932494c360523476d
Jason <bjason@gmail.com>
2014-12-24 01:19:44 -0800

    Create spin.cs

---
+++ spin.cs
@@ -0,0 +1,16 @@
+ using UnityEngine;
+using System.Collections;
+
+public class spin : MonoBehaviour {
+	public float rate = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (0, rate, 0);
+	
+	}
+}
