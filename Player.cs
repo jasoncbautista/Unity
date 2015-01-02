ec7b53f79322e269cab70d97602436553975a382
Jason <bjason@gmail.com>
2015-01-01 23:19:34 -0800

    Create Player.cs

---
+++ Player.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public float speed = 10f;
+	public Vector2 maxVelocity = new Vector2(2,3);
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var forceX = 0f;
+		var forceY = 0f;
+		
+		var absVelX = Mathf.Abs (rigidbody2D.velocity.x);
+		
+		
+		
+		if (Input.GetKey ("right")) {
+			if(absVelX < maxVelocity.x)
+				forceX = speed;
+		} else if (Input.GetKey ("left")) {
+			
+			if(absVelX < maxVelocity.x)
+				forceX = -speed;
+			
+		}
+		
+		
+		rigidbody2D.AddForce(new Vector2(forceX, forceY));
+		
+		
+		
+	}
+}
